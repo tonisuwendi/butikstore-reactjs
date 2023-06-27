80e21172b53b9cefd5600182401c5ed656c862f7
Toni Suwendi <44643523+tonisuwendi@users.noreply.github.com>
2023-06-27 22:09:57 +0700

    change PUBLIC_URL

--- src/lib/endpoints/common.js
+++ src/lib/endpoints/common.js
@@ -1,7 +1,7 @@
 const DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 const LOCAL_URL = 'http://localhost:5000/';
-const PUBLIC_URL = 'https://someapi.buntomart.co/butikstore/';
+const PUBLIC_URL = 'https://butikstore-api.antasena.site/';
 
 const common = {
   DOMAIN_API: DEVELOPMENT ? LOCAL_URL : PUBLIC_URL,
